aed93380b300109a43f9463abebf92c86f6607b6
Linghao Zhang <zlhdnc1994@gmail.com>
2016-04-10 16:29:22 +0800

    add many features

--- home-depot/slave.py
+++ home-depot/slave.py
@@ -1,4 +1,4 @@
-# -*- encoding:utf-8 -*-
+# -*- encoding:ISO-8859-1 -*-
 import warnings
 warnings.filterwarnings("ignore")
 import time
@@ -92,10 +92,11 @@ class cust_regression_vals(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, hd_searches):
-        d_col_drops = ['id', 'relevance', 'search_term', 'product_title',
-                       'product_description', 'brand',
-                       'lev_dist_to_product_title_min', 'lev_dist_to_product_title_max',
-                       'lev_dist_to_product_title_sum'
+        d_col_drops = ['id', 'product_uid', 'relevance',
+                       'search_term', 'search_term_stemmed',
+                       'product_title', 'product_title_stemmed',
+                       'product_description', 'product_description_stemmed',
+                       'brand', 'brand_stemmed'
                        ]
         hd_searches = hd_searches.drop(d_col_drops, axis=1).values
         return hd_searches
@@ -114,7 +115,7 @@ class cust_txt_col(BaseEstimator, TransformerMixin):
 
 if __name__ == '__main__':
     num_train = 74067
-    df_all = pd.read_csv('df_.csv', index_col=0)
+    df_all = pd.read_csv('df_start_over.csv', encoding='ISO-8859-1', index_col=0)
     df_train = df_all.iloc[:num_train]
     df_test = df_all.iloc[num_train:]
     id_test = df_test['id']
@@ -136,20 +137,17 @@ if __name__ == '__main__':
             transformer_list=[
                             ('cst', cust_regression_vals()),
                             ('txt1', pipeline.Pipeline(
-                                [('s1', cust_txt_col(key='search_term')), ('tfidf1', tfidf), ('tsvd1', tsvd)])),
+                                [('s1', cust_txt_col(key='search_term_stemmed')), ('tfidf1', tfidf), ('tsvd1', tsvd)])),
                             ('txt2', pipeline.Pipeline(
-                                [('s2', cust_txt_col(key='product_title')), ('tfidf2', tfidf), ('tsvd2', tsvd)])),
+                                [('s2', cust_txt_col(key='product_title_stemmed')), ('tfidf2', tfidf), ('tsvd2', tsvd)])),
                             ('txt3', pipeline.Pipeline(
-                                [('s3', cust_txt_col(key='product_description')), ('tfidf3', tfidf), ('tsvd3', tsvd)])),
-                            ('txt4', pipeline.Pipeline(
-                                [('s4', cust_txt_col(key='brand')), ('tfidf4', tfidf), ('tsvd4', tsvd)]))
+                                [('s3', cust_txt_col(key='product_description_stemmed')), ('tfidf3', tfidf), ('tsvd3', tsvd)]))
                             ],
             transformer_weights={
                 'cst': 1.0,
                 'txt1': 0.5,
                 'txt2': 0.25,
                 'txt3': 0.5,
-                'txt4': 0.5
             },
             # n_jobs = -1
         )),
@@ -163,20 +161,13 @@ if __name__ == '__main__':
     #               'xgb__colsample_bytree': [0.9, 0.95],
     #               'xgb__subsample': [0.8, 0.85, 0.9]
     #               }
-    model = grid_search.GridSearchCV(estimator=clf, param_grid=param_grid, n_jobs=1, cv=4, verbose=20, scoring=RMSE)
+    model = grid_search.GridSearchCV(estimator=clf, param_grid=param_grid, n_jobs=1, cv=5, verbose=20, scoring=RMSE)
     model.fit(X_train, y_train)
 
     print("Best parameters found by grid search:")
     print(model.best_params_)
     print("Best CV score:")
-    print(model.best_score_)
-    for p in ['txt1', 'txt2', 'txt3', 'txt4']:
-        print(p + " weights:")
-        print(clf.get_params()['union'].transformer_weights[p])
-    print('SVD n_components:')
-    print(tsvd.get_params()['n_components'])
-    
-    exit(0)
+    print(-model.best_score_)
 
     # from sklearn import cross_validation
     # sub_clf = model.best_estimator_
