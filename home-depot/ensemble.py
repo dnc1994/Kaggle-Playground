97589609c1893f78e86cff62cdb958bccbf45ed3
Linghao Zhang <zlhdnc1994@gmail.com>
2016-04-21 01:18:01 +0800

    fix ensemble

--- home-depot/ensemble.py
+++ home-depot/ensemble.py
@@ -3,35 +3,82 @@ start_time = time.time()
 import numpy as np
 import pandas as pd
 from sklearn.cross_validation import KFold
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestRegressor, ExtraTreesRegressor
+from sklearn.ensemble import RandomForestRegressor, ExtraTreesRegressor, GradientBoostingRegressor
+from sklearn.base import BaseEstimator
 
 
-class Ensemble(object):
+class Ensemble(BaseEstimator):
     def __init__(self, n_folds, stacker, base_models):
         self.n_folds = n_folds
         self.stacker = stacker
         self.base_models = base_models
+
+    def fit(self, X, y):
+        X = np.array(X)
+        y = np.array(y)
+        folds = list(KFold(len(y), n_folds=self.n_folds, shuffle=True, random_state=2016))
+        S_train = np.zeros((X.shape[0], len(self.base_models)))
+
+        for i, clf in enumerate(self.base_models):
+
+            print('Fitting For Base Model #%d / %d ---', i+1, len(self.base_models))
+            for j, (train_idx, test_idx) in enumerate(folds):
+
+                print('--- Fitting For Fold %d / %d ---', j+1, self.n_folds)
+
+                X_train = X[train_idx]
+                y_train = y[train_idx]
+                X_holdout = X[test_idx]
+                # y_holdout = y[test_idx]
+                clf.fit(X_train, y_train)
+                y_pred = clf.predict(X_holdout)[:]
+                S_train[test_idx, i] = y_pred
+
+                print('Elapsed: %s minutes ---' % round(((time.time() - start_time) / 60), 2))
+
+            print('Elapsed: %s minutes ---' % round(((time.time() - start_time) / 60), 2))
+
+        print('--- Base Models Trained: %s minutes ---' % round(((time.time() - start_time) / 60), 2))
+
+        clf = self.stacker
+        clf.fit(S_train, y)
+
+        print('--- Stacker Trained: %s minutes ---' % round(((time.time() - start_time) / 60), 2))
+
+    def preidct(self, X):
+        X = np.array(X)
+        folds = list(KFold(len(X), n_folds=self.n_folds, shuffle=True, random_state=2016))
+        S_test = np.zeros((X.shape[0], len(self.base_models)))
+
+        for i, clf in enumerate(self.base_models):
+            S_test_i = np.zeros((X.shape[0], len(folds)))
+            for j, (train_idx, test_idx) in enumerate(folds):
+                S_test_i[:, j] = clf.predict(X)[:]
+            S_test[:, i] = S_test_i.mean(1)
+
+        clf = self.stacker
+        y_pred = clf.predict(S_test)[:]
+        return y_pred
 
     def fit_predict(self, X, y, T):
         X = np.array(X)
         y = np.array(y)
         T = np.array(T)
 
-        folds = list(KFold(y, self.n_folds))
+        folds = list(KFold(len(y), n_folds=self.n_folds, shuffle=True, random_state=2016))
 
         S_train = np.zeros((X.shape[0], len(self.base_models)))
         S_test = np.zeros((T.shape[0], len(self.base_models)))
 
         for i, clf in enumerate(self.base_models):
 
-            print('Fitting For Base Model #%d / %d ---', i+1, len(self.base_models))
+            print('Fitting For Base Model #{0} / {1} ---'.format(i+1, len(self.base_models)))
 
             S_test_i = np.zeros((T.shape[0], len(folds)))
 
             for j, (train_idx, test_idx) in enumerate(folds):
 
-                print('--- Fitting For Fold %d / %d ---', j+1, self.n_folds)
+                print('--- Fitting For Fold #{0} / {1} ---'.format(j+1, self.n_folds))
 
                 X_train = X[train_idx]
                 y_train = y[train_idx]
@@ -52,11 +99,10 @@ class Ensemble(object):
 
         clf = self.stacker
         clf.fit(S_train, y)
-        clf.predict(S_train)
 
         print('--- Stacker Trained: %s minutes ---' % round(((time.time() - start_time) / 60), 2))
 
-        y_pred = clf.predict(S_test)[:, 1]
+        y_pred = clf.predict(S_test)[:]
 
         return y_pred
 
@@ -66,6 +112,9 @@ def main(input='df_new_419_3.csv'):
     num_train = 74067
     df_train = df_all.iloc[:num_train]
     df_test = df_all.iloc[num_train:]
+
+    # df_train = df_all.iloc[:50]
+    # df_test = df_all.iloc[50:100]
 
     id_test = df_test['id']
     y_train = df_train['relevance'].values
@@ -88,13 +137,24 @@ def main(input='df_new_419_3.csv'):
         RandomForestRegressor(
             n_jobs=1, random_state=2016, verbose=1,
             n_estimators=500, max_features=10
+        ),
+        ExtraTreesRegressor(
+            n_jobs=1, random_state=2016, verbose=1,
+            n_estimators=500, max_features=10
+        ),
+        GradientBoostingRegressor(
+            random_state=2016, verbose=1,
+            n_estimators=300, max_features=10, max_depth=10,
+            learning_rate=0.05, subsample=0.8
         )
-        # ,
-        # ExtraTreesRegressor()
     ]
     ensemble = Ensemble(
         n_folds=5,
-        stacker=LogisticRegression(C=0.1, penalty='l1'),
+        stacker=GradientBoostingRegressor(
+            random_state=2016, verbose=1,
+            n_estimators=200,
+            learning_rate=0.05, subsample=0.8
+        ),
         base_models=base_models
     )
 
@@ -104,7 +164,7 @@ def main(input='df_new_419_3.csv'):
             y_pred[i] = 1.0
         if y_pred[i] > 3.0:
             y_pred[i] = 3.0
-    pd.DataFrame({'id': id_test, 'relevance': y_pred}).to_csv('submission_rfr.csv', index=False)
+    pd.DataFrame({'id': id_test, 'relevance': y_pred}).to_csv('submission_ensemble.csv', index=False)
 
     print('--- Submission Generated: %s minutes ---' % round(((time.time() - start_time) / 60), 2))
 
